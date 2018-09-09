40c2d2bf52528c1b9f1b8778db12b09e65706ca5
a-rutnam <a.rutnam@gmail.com>
2018-09-09 15:16:58 +1000

    bqct mock login and quiz index show page working

---
+++ src/views/QuizShow.vue
@@ -0,0 +1,36 @@
+<template>
+    <div id="quiz">
+quiz
+<pre> {{quizzes}}
+{{questions}}
+</pre>
+    </div>
+</template>
+
+<script>
+  import quizzes from '@/assets/quiz_data/quizzes.json'
+  import questions from '@/assets/quiz_data/questions.json'
+    export default {
+        name: 'QuizzesIndex',
+        data() {
+            return {
+              quizzes,
+              questions
+            }
+        },
+        methods: {
+
+        }
+    }
+</script>
+
+<style scoped>
+    #quiz {
+        width: 500px;
+        border: 1px solid #CCCCCC;
+        background-color: #FFFFFF;
+        margin: auto;
+        margin-top: 200px;
+        padding: 20px;
+    }
+</style>
---
+++ src/views/QuizzesIndex.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="quiz">
+Quizzes
+
+<ul>
+  <li v-for="quiz in quizzes">{{quiz.title}}</li>
+</ul>
+
+
+
+  </div>
+</template>
+
+<script>
+  import quizzes from '@/assets/quiz_data/quizzes.json'
+    export default {
+        name: 'QuizzesIndex',
+        data() {
+            return {
+              quizzes: quizzes.quizzes
+            }
+        },
+        methods: {
+
+        }
+    }
+</script>
+
+<style scoped>
+    #quiz {
+        width: 500px;
+        border: 1px solid #CCCCCC;
+        background-color: #FFFFFF;
+        margin: auto;
+        margin-top: 200px;
+        padding: 20px;
+    }
+</style>
